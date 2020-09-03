0724446cef193a6e6bf9c925aa5b9a6cce8295c2
cgschu1tz <cgschultz@ksu.edu>
2020-09-03 16:09:22 -0500

    Forgot to update docs for AretinoAppleJuice.

--- Data/Drinks/AretinoAppleJuice.cs
+++ Data/Drinks/AretinoAppleJuice.cs
@@ -1,8 +1,11 @@
-﻿// Author: Chris Schultz
+﻿/*
+ * Author: Chris Schultz
+ * Class name: AretinoAppleJuice.cs
+ * Purpose: Defines a class for Aretino Apple Juice
+ */
 using BleakwindBuffet.Data.Enums;
 using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace BleakwindBuffet.Data.Drinks
 {
@@ -11,9 +14,9 @@ namespace BleakwindBuffet.Data.Drinks
     /// </summary>
     public class AretinoAppleJuice
     {
-        /// <summary>
+        /// <value>
         /// The price of this item in USD.
-        /// </summary>
+        /// </value>
         /// <exception cref="NotImplementedException">
         /// <see cref="Size"/> is not a valid size
         /// </exception>
@@ -30,9 +33,9 @@ namespace BleakwindBuffet.Data.Drinks
             }
         }
 
-        /// <summary>
+        /// <value>
         /// How many calories this item contains.
-        /// </summary>
+        /// </value>
         /// <exception cref="NotImplementedException">
         /// <see cref="Size"/> is not a valid size
         /// </exception>
@@ -49,15 +52,15 @@ namespace BleakwindBuffet.Data.Drinks
             }
         }
 
-        /// <summary>
+        /// <value>
         /// The size of this item.
-        /// </summary>
+        /// </value>
         public Size Size { get; set; } = Size.Small;
 
-        /// <summary>
+        /// <value>
         /// A list of instructions to follow when preparing this item
         /// (e.g. "Hold mayo" or "Hold ice").
-        /// </summary>
+        /// </value>
         public List<string> SpecialInstructions
         {
             get {
@@ -69,11 +72,15 @@ namespace BleakwindBuffet.Data.Drinks
             }
         }
 
-        // Ingredients:
-        // Set to true to include in this item and false to exclude them.
+        /// <value>
+        /// <c>true</c> if this ingredient is to be included and <c>false</c> if it is to be excluded.
+        /// </value>
         public bool Ice { get; set; } = false;
 
-        /// <returns>A string containing the name of this item.</returns>
+        /// <summary>
+        /// Converts this item to its string representation.
+        /// </summary>
+        /// <returns>A string containing the name of the item.</returns>
         public override string ToString() => $"{Size} Arentino Apple Juice";
     }
 }
