c3582c4ccd7bc142e305eaaa00efa39d50f3ddd5
cgschu1tz <cgschultz@ksu.edu>
2020-09-12 16:24:41 -0500

    Created Side

---
+++ Data/Sides/Side.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Author: Chris Schultz
+ * Class name: Side.cs
+ * Purpose: Defines an abstract base class for all sides
+ */
+using BleakwindBuffet.Data.Enums;
+using System.Collections.Generic;
+
+namespace BleakwindBuffet.Data.Sides
+{
+    /// <summary>
+    /// Abstract base class for all sides
+    /// </summary>
+    public abstract class Side
+    {
+        /// <summary>
+        /// The size of this item.
+        /// </summary>
+        public abstract Size Size { get; set; }
+
+        /// <summary>
+        /// The price of the item in USD.
+        /// </summary>
+        public abstract double Price { get; }
+
+        /// <summary>
+        /// How many calories the item contains.
+        /// </summary>
+        public abstract uint Calories { get; }
+
+        /// <summary>
+        /// A list of instructions to follow when preparing the item
+        /// (e.g. "Hold mayo" or "Hold ice").
+        /// </summary>
+        public abstract List<string> SpecialInstructions { get; }
+    }
+}
